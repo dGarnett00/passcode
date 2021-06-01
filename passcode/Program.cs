9e52a103bca5f0b640b6366ba2913620c8725c01
bruh man 5th floor <davonta2222@gmail.com>
2021-05-31 18:00:58 -0700

    Add project files.

---
+++ passcode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = "";
+            while(code != "secret")
+            {
+
+            }
+            Console.WriteLine("what is the passcode");
+             code = Console.ReadLine();
+
+            if(code =="secret")
+            {
+                Console.WriteLine("Authenticated");
+            }
+            else if(code !="secret")
+            {
+                Console.WriteLine("Not Authenticated");
+            }
+        }
+    }
+}
